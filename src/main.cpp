571e82acdfff32224d15c0d38d47860d0bf75840
use25 <uselessguypvz@gmail.com>
2026-07-07 19:45:33 +0700

    Creating temp json and loading json

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,35 @@
+#include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <vector>
+#include "rapidjson/document.h"
+
+void LoadJSON(const wchar_t* i_path)
+{
+    std::ifstream file(i_path, std::ios::binary);
+    if (!file.is_open())
+    {
+        std::wcout << L"File not opened: " << i_path << std::endl;
+        return;
+    }
+
+    auto size = std::filesystem::file_size(i_path) + 1;
+    char* buffer = new char[size] {0};
+    file.read(buffer, size);
+
+    rapidjson::Document document;
+    document.Parse(buffer);
+
+    if (document.HasParseError())
+    {
+        std::cout << "JSON parse error at offset: " << document.GetErrorOffset() << std::endl;
+    }
+    delete[] buffer;
+    file.close();
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    LoadJSON(L"../../data/test.json");
+    return 0;
 }
