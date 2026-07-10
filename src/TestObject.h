dba1c9fa034a5c9c1bd805a10dc685cce8f88a42
Huy Nguyen Quang <huy.nguyenquang@gameloft.com>
2026-07-10 18:17:17 +0700

    Temp

--- src/TestObject.h
+++ src/TestObject.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "rapidjson/reader.h"
 #include <string>
 
 struct TestObject {
@@ -9,4 +10,84 @@ struct TestObject {
 	char m_i;
 	float m_pi;
 	std::vector<char> m_a;
+};
+
+struct TestObjectHandler : public rapidjson::BaseReaderHandler<rapidjson::UTF8<>, TestObjectHandler>
+{
+    enum class Key { None, Hello, T, F, N, I, Pi, A };
+
+    TestObject* mo_obj;
+    Key m_currentKey = Key::None;
+    bool m_inArray = false;
+
+    TestObjectHandler(TestObject& i_obj) : rapidjson::BaseReaderHandler<rapidjson::UTF8<>, TestObjectHandler>()
+        , mo_obj(&i_obj)
+    {
+    }
+
+    bool Key(const char* str, rapidjson::SizeType length, bool /*copy*/)
+    {
+        std::string_view key(str, length);
+        if (key == "hello")     m_currentKey = Key::Hello;
+        else if (key == "t")    m_currentKey = Key::T;
+        else if (key == "f")    m_currentKey = Key::F;
+        else if (key == "n")    m_currentKey = Key::N;
+        else if (key == "i")    m_currentKey = Key::I;
+        else if (key == "pi")   m_currentKey = Key::Pi;
+        else if (key == "a")    m_currentKey = Key::A;
+        else                    m_currentKey = Key::None; // unknown key, ignore its value
+        return true;
+    }
+
+    bool String(const char* str, rapidjson::SizeType length, bool /*copy*/)
+    {
+        if (m_currentKey == Key::Hello)
+        {
+            mo_obj->m_hello.assign(str, length);
+        }
+        return true;
+    }
+
+    bool Bool(bool b)
+    {
+        if (m_currentKey == Key::T) mo_obj->m_t = b;
+        else if (m_currentKey == Key::F) mo_obj->m_f = b;
+        return true;
+    }
+
+    bool Null()
+    {
+        if (m_currentKey == Key::N) mo_obj->m_n = 0;
+        return true;
+    }
+
+    bool Double(double d)
+    {
+        if (m_currentKey == Key::Pi) mo_obj->m_pi = static_cast<float>(d);
+        return true;
+    }
+
+    bool Int(int i)
+    {
+        if (m_inArray) mo_obj->m_a.push_back(static_cast<char>(i));
+        else if (m_currentKey == Key::I) mo_obj->m_i = static_cast<char>(i);
+        return true;
+    }
+    bool Uint(unsigned u) { return Int(static_cast<int>(u)); }
+
+    bool StartArray()
+    {
+        if (m_currentKey == Key::A) m_inArray = true;
+        return true;
+    }
+
+    bool EndArray(rapidjson::SizeType /*count*/)
+    {
+        m_inArray = false;
+        m_currentKey = Key::None;
+        return true;
+    }
+
+    bool StartObject() { return true; }
+    bool EndObject(rapidjson::SizeType) { return true; }
 };
